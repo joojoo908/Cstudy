bbeb0532c96a02f03b1a0989037d6cc187e1970e
unknown <kooko@DESKTOP-O83PM16.lgcns.com>
2019-12-30 20:01:51 +0900

    card game2

--- studyC001/studyc30.c
+++ studyC001/studyc30.c
@@ -13,8 +13,14 @@ typedef struct linkedCard{
     int cnt;
 }linkedCard;
 
+typedef struct user{
+    char name[10];
+    linkedCard hand;
+}user;
+
 void init(linkedCard * dack){
     card *it;
+    dack->cnt = 0;
     char c[4]={'s','h','d','c'};
     (* dack).head = (card *)malloc(sizeof(card));
     it=(* dack).head;
@@ -54,18 +60,83 @@ void supple(linkedCard * dack){
 void printcard(linkedCard *dack){
     card *it;
     it=(* dack).head;
+    int kount=0;
     while(it->next!=NULL){
+        kount+=1;
         it=it->next;
-        printf("card %c %d \n",it->shape,it->num);
+        printf("%d:%c %d, ",kount,it->shape,it->num);
     }
 }
 
+void takeCard(user *p, linkedCard *dack){
+    for(int i=0;i<3;i++){
+        p[i].hand.head = (card *)malloc(sizeof(card));
+    }
+    for(int j=0;j<3;j++){
+            (p[j].hand.head)->next=((* dack).head)->next;
+            ((* dack).head)->next=(((* dack).head)->next)->next;
+            ((p[j].hand.head)->next)->next=NULL;
+            (* dack).cnt=(* dack).cnt-1;
+        }
+    for(int i=0;i<6;i++){
+        for(int j=0;j<3;j++){
+            card *tmp;
+            tmp=((* dack).head)->next;
+            ((* dack).head)->next=(((* dack).head)->next)->next;
+            tmp->next=(p[j].hand.head)->next;
+            (p[j].hand.head)->next=tmp;
+            (* dack).cnt=(* dack).cnt-1;
 
+        }
+        
+    }
+
+}
+
+int check_shape(card player, card pan){
+    if (player.shape == pan.shape)
+        return 1;
+    else
+        return 0;   
+}
+int check_num(card player, card pan){
+    if(player.num == pan.num)
+        return 1;
+    else
+        return 0;
+    
+}
+
+void Start_game(linkedCard *dack ,user *p,linkedCard *pan){
+    init(dack);
+    supple(dack);
+    takeCard(p,dack);
+    (pan->head)->next=(dack->head)->next;
+    (dack->head)->next=((dack->head)->next)->next;
+
+}
 
 int main(){
     linkedCard a;
+    user p[3];
+    linkedCard pan;
+    pan.head =(card*)malloc(sizeof(card));
+
     init(&a);
     supple(&a);
-    printcard(&a);
+    takeCard(p,&a);
+    pan.head->next=a.head->next;
+    a.head->next=(a.head->next)->next;
+
+    printf("Front:%c %d",pan.head->next->shape,pan.head->next->num);
+    printf("\ndack_list:%d", a.cnt);
+    printf("\nuser1:");
+    printcard(&(p[0].hand));
+    printf("\nuser2:");
+    printcard(&(p[1].hand));
+    printf("\nuser3:");
+    printcard(&(p[2].hand));
+
+    printf("\n%c",k);
 
 }
